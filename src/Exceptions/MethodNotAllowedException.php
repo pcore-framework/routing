9af5e3229a78184ca0635c75546143d49281d912
Magomedcoder <info@magomedcoder.ru>
2022-06-06 23:26:34 +0300

    Added exception MethodNotAllowedException

---
+++ src/Exceptions/MethodNotAllowedException.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PCore\Routing\Exceptions;
+
+use Exception;
+
+/**
+ * Class MethodNotAllowedException
+ * @package PCore\Routing\Exceptions
+ * https://github.com/pcore-framework/routing
+ */
+class MethodNotAllowedException extends Exception
+{
+}
